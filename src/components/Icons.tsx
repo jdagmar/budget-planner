20ef2142dcb91300e0e62c0326221497d37f0dc9
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-02-23 14:44:43 +0100

    Add icon component

---
+++ src/components/Icons.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const AddIcon = () => {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className=""
+    >
+      <path d="M12 5v14M5 12h14" />
+    </svg>
+  );
+};
