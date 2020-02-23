61eb49ff3da507db8d96c3ee89376d33e51c64b1
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-02-23 14:46:42 +0100

    Add barrel file

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+export { Input } from './Input';
+export { Header } from './Header';
+export { ExpenseSummary } from './ExpenseSummary';
+export { Button } from './Button';
+export { AddIcon } from './Icons';
+export { ExpenseForm } from './ExpenseForm';
+export { ExpenseList } from './ExpenseList';
