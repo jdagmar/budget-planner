0ecf9f8583ae1d5b2e00003ed1b809ee5a61d918
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-03-09 20:24:33 +0100

    Updates

---
+++ precache-manifest.6d09299ced4d634ddf12765ddf4c52f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879c9f4bfd1b4089406ccad2d0b09d75",
+    "url": "/budget-planner/index.html"
+  },
+  {
+    "revision": "56c69ab457b2ffa96418",
+    "url": "/budget-planner/static/css/main.57d005de.chunk.css"
+  },
+  {
+    "revision": "31658d725bc9b4eeac62",
+    "url": "/budget-planner/static/js/2.fa129672.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/budget-planner/static/js/2.fa129672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56c69ab457b2ffa96418",
+    "url": "/budget-planner/static/js/main.0a5172cc.chunk.js"
+  },
+  {
+    "revision": "bded1c801414eb3c2749",
+    "url": "/budget-planner/static/js/runtime-main.96318483.js"
+  }
+]);
