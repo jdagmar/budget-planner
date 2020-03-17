8ddb81a986c6024ddf6dceb5af9ce669a3d187f0
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-03-17 22:45:32 +0100

    Add erroricon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -75,3 +75,21 @@ export const CloseIcon = (props: Props) => {
     </svg>
   );
 };
+
+export const ErrorIcon = (props: Props) => {
+  return (
+    <svg
+      width={16}
+      height={16}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={props.cssClass}
+    >
+      <path d="M7.86 2h8.28L22 7.86v8.28L16.14 22H7.86L2 16.14V7.86L7.86 2zM12 8v4M12 16h.01" />
+    </svg>
+  );
+};
