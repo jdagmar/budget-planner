d6c037f9ac60ad9789f6752ad12209ed02217569
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-03-17 22:45:54 +0100

    Add progressbar

--- src/components/ExpenseSummary.tsx
+++ src/components/ExpenseSummary.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Expense, SetIncomeForm } from '../State';
+import { GetIcon } from '../helpers/GetIcon';
 
 type Props = {
   expenses: Array<Expense>;
@@ -7,21 +8,57 @@ type Props = {
 };
 
 export const ExpenseSummary = (props: Props) => {
-  const expenses = props.expenses.map(expense => +expense.cost);
-
   const sumUpExpenses = (expenses: Array<number>) => {
     return expenses.reduce((x, y) => x + y, 0);
   };
 
   const calcAccountBalance = (income: string = '0') => {
-    return parseInt(income) - sumUpExpenses(expenses);
+    if (!isNaN(parseInt(income))) {
+      return parseInt(income) - sumUpExpenses(expenses);
+    }
+    return 'unable to calculate';
   };
 
+  const calcPrecentageSpent = (expenses: number, income: string = '0') => {
+    return (expenses / parseInt(income)) * 100;
+  };
+
+  const expenses = props.expenses.map(expense => +expense.cost);
+  const percentageSpent = calcPrecentageSpent(
+    sumUpExpenses(expenses),
+    props.setIncomeForm.income
+  );
+  const accountBalance = calcAccountBalance(props.setIncomeForm.income);
+
   return (
-    <div className="mt-2 font-roboto">
-      <p>Sum of expenses: {sumUpExpenses(expenses)} SEK</p>
-      <p>
-        Account balance: {calcAccountBalance(props.setIncomeForm.income)} SEK{' '}
+    <div className="mt-2 font-overpass">
+      <div className="shadow w-full bg-grey-light rounded">
+        <div
+          className={`${
+            percentageSpent > 100
+              ? 'bg-red-500'
+              : percentageSpent < 100
+              ? 'bg-peach-500'
+              : 'bg-gray-400'
+          } text-xs leading-none py-2 text-center max-w-full`}
+          style={{
+            width: percentageSpent + '%',
+          }}
+        >
+          {sumUpExpenses(expenses)}
+        </div>
+      </div>
+
+      <p className="flex justify-end items-center mt-4 text-sm">
+        Total:
+        <span
+          className={`${
+            accountBalance < 0 ? 'text-red-700' : ''
+          } px-2 flex items-center`}
+        >
+          <span>{accountBalance}</span>
+          <span>{accountBalance < 0 && GetIcon('error', 'ml-2')}</span>
+        </span>
       </p>
     </div>
   );
