5487aac67a240a723e5ccfa40683984fc38f4916
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-04-16 19:01:34 +0200

    New heading style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Header = () => {
   return (
-    <header className="mb-2 px-3 py-4 bg-peach-500 text-center flex justify-center">
+    <header className="mb-2 px-3 py-4 border-gray-200 border-b  flex ">
       <h1 className="font-overpass">Budget Planner</h1>
       <span role="img" className="ml-3" aria-label="money flying away">
         💸
