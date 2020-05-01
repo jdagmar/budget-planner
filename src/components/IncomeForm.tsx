ac61199d144fa36f3d59da5d0041e5ff2b725f23
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-05-01 22:50:06 +0200

    Add currency field

--- src/components/IncomeForm.tsx
+++ src/components/IncomeForm.tsx
@@ -9,23 +9,47 @@ type Props = {
 
 export const IncomeForm = (props: Props) => {
   return (
-    <>
-      <Input
-        label="income"
-        type="string"
-        icon="money"
-        value={props.setIncomeForm.income?.toString() || ''}
-        onChange={newIncome => {
-          props.onChange({
-            ...props.setIncomeForm,
-            income: newIncome,
-            error: isNaN(parseInt(newIncome))
-              ? 'Income should be a number'
-              : undefined,
-          });
-        }}
-      />
-      <p className="text-sm text-red-700  mb-2">{props.setIncomeForm.error}</p>
-    </>
+    <div className="flex">
+      <div className="w-8/12 mr-2">
+        <Input
+          label="income"
+          type="string"
+          icon="money"
+          value={props.setIncomeForm.income?.toString() || ''}
+          onChange={newIncome => {
+            props.onChange({
+              ...props.setIncomeForm,
+              income: newIncome,
+              incomeFormatError: isNaN(parseInt(newIncome))
+                ? 'Income should be a number'
+                : undefined,
+            });
+          }}
+        />
+        <p className="text-sm text-red-700 mb-2">
+          {props.setIncomeForm.incomeFormatError}
+        </p>
+      </div>
+      <div className="w-4/12">
+        <Input
+          label="currency"
+          type="string"
+          icon="label"
+          value={props.setIncomeForm.currency || ''}
+          onChange={newCurrency => {
+            props.onChange({
+              ...props.setIncomeForm,
+              currency: newCurrency,
+              currencyFormatError: isNaN(parseInt(newCurrency))
+                ? undefined
+                : 'Income should be a string',
+            });
+          }}
+        />
+        <p className="text-sm text-red-700 mb-2">
+          {props.setIncomeForm.currencyFormatError}
+        </p>
+      </div>
+    </div>
   );
 };
