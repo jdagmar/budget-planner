e17b8962414c5b7dae1dc64f5b4e48c78a118724
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-05-03 13:09:41 +0200

    Make input bg transparent

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -24,7 +24,7 @@ export const Input = (props: Props) => {
         <input
           id={id}
           type={props.type}
-          className={`font-roboto border border-gray-800 p-2 w-full rounded-sm  ${
+          className={`font-roboto bg-transparent border border-gray-800 p-2 w-full rounded-sm  ${
             props.icon ? 'pl-8' : 'pl-2'
           }`}
           value={props.value}
